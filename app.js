ee0fe08e4ece637b7450964bd1eed4125fcec164
a.mihnov <a.mihnov@anlax.org>
2023-11-06 09:54:20 +0300

    add style for dialog window

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const dialog = document.querySelector('dialog');
 
 const plus = document.querySelector('.plus');
 
+const close = document.querySelector('.close');
+
 plus.addEventListener('click', () => {
     dialog.showModal();
 })
+
+close.addEventListener('click', () => {
+    dialog.close();
+})
+
