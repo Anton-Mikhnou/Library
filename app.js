81ceaf3de1153c4b40b3218ebb6c1d187332468e
a.mihnov <a.mihnov@anlax.org>
2023-11-08 20:37:42 +0300

    Can add card on the screen

--- app.js
+++ app.js
@@ -12,3 +12,38 @@ close.addEventListener('click', () => {
     dialog.close();
 })
 
+const workPlace = document.querySelector('.workPlace');
+
+const author = document.getElementById('author');
+const title = document.getElementById('title');
+const pages = document.getElementById('pages');
+
+function addCard() {
+    const card = document.createElement('div');
+    card.textContent = author.value;
+    card.classList.add('item');
+    workPlace.appendChild(card);
+}
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addCard();
+    dialog.close();
+    addBookToLibrary()
+    console.log('1',myLibrary)
+    form.reset();
+})
+
+const myLibrary = [];
+
+function Book(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+    myLibrary.push(new Book(author.value, title. value, pages.value))
+}
